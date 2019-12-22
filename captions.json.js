6ccee0bc1450e0f0887a62b7dbe0cb6891aa5aff
shoh38 <shoh38@outlook.jp>
2019-12-22 19:21:53 +0900

    初回コミット

---
+++ captions.json.js
@@ -0,0 +1,8 @@
+﻿let captions = [
+{ time: -0.098, text: "LEFT_DOWN_0.002" },
+{ time: 3.489, text: "LEFT_DOWN_3.589" },
+{ time: 7.238, text: "LEFT_DOWN_7.338" },
+{ time: 11.94, text: "LEFT_DOWN_12.04" },
+{ time: 17.37, text: "LEFT_DOWN_17.47" },
+{ time: 19.306, text: "LEFT_DOWN_19.406" }
+]
